cb6a24f13b0ebb7ee9c76a62954bc36127475118
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-05-31 22:00:04 +0700

    Initial Commit

---
+++ Rive Animations/Rive_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rive_AnimationsApp.swift
+//  Rive Animations
+//
+//  Created by TheGIZzz on 31/5/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Rive_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
